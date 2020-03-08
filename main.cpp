5623a1746b81230c48035648238885bf6059cf43
Evgeny <jekasdobnov@yandex.ru>
2020-03-09 00:46:11 +0300

    argc argv

--- main.cpp
+++ main.cpp
@@ -1,12 +1,24 @@
 #include<stdio.h>
 #include<iostream>
 #include<vector>
+#include <stdlib.h>
 using namespace std;
 
-int m1, m2;
+int m1, m2, n;
+double mu;
 int T=1, X1=1, X2=1;
 
-
+/*
+    Область
+                 ___________
+                |___|___|___|    
+                |___|___|___|
+     ___________|___|___|___|___________    m1,i
+    |___|___|___|___|___|___|___|___|___|    
+    |___|___|___|___|___|___|___|___|___|
+    |___|___|___|___|___|___|___|___|___|
+                    m2,j
+*/
 class Matrix
 {
 public:
@@ -14,14 +26,24 @@ public:
     vector<vector<int>> Ind;
     int size;
 
+
+    Matrix();
+
     Matrix (int n): size(n)
     {
         Mat.resize(n);
         Ind.resize(n);
     }
+    
+    void resize (int n)
+    {
+        size = n;
+        Mat.resize(n);
+        Ind.resize(n);
+    }
 
 };
-
+Matrix Mat(1);
 int ij2k (int i, int j)
 {
     if (i <= m1/3)
@@ -47,19 +69,27 @@ void k2ij(int k, int& i, int& j)
         i = m1/3 + 1;
         j = m2/3;
         k -= (m2 + 1) * (m1 / 3 + 1);
+
         i += k / (m2/3 + 1);
         j += k % (m2/3 + 1);
     }
     
 }
 
-int main()
+int main(int argc, char *argv[])
 {
-    Matrix a(20);
-    m1=m2=9;
-    int i,j,k=47;
-    k2ij(k,i,j);
-    cout << i << " " << j << endl;
-    printf("hhahah\n");
+    if (argc != 5 )
+    {
+        cout << "wrong argc\n";
+        return -1;
+    }
+    m1 = atoi(argv[1]);
+    m2 = atoi(argv[2]);
+    n = atoi(argv[3]);
+    mu = atof(argv[4]);
+    Mat.resize(ij2k(2*m1/3, 2*m2/3));
+
+    
+    
     return 0;
 }
